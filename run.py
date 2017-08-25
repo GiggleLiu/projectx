f519b9cc2d10675fed25ccd686de72c80e251c44
Roger-luo <hiroger@qq.com>
2017-08-25 17:16:37 +0800

    add fire

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
+import fire
 import numpy as np
-
 from controller import *
 
 np.random.seed(2)
-#run_rtheta(J2=0.8, nsite=10)
-#run_ed_msr(J2=0.8, nsite=16)
-scale_ed_msr(NJ2=51, size=(4,4))
+
+if __name__ == '__main__':
+    fire.Fire()
--- run_bench.py
+++ run_bench.py
@@ -1,20 +1,24 @@
 import numpy as np
-
+import fire
 from controller import *
 
 np.random.seed(2)
-# depth benchmark
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 16])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 16, 8])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 32, 16, 8])
-# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 256, 200, 64, 32, 32, 32, 32, 32, 32, 32, 32, 16, 16, 8])
 
-# shape benchmark
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 32, 64])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 32, 16, 32])
-run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 32, 64])
-#run_ed_msr(J2=0.8, nsite=16)
-#scale_ed_msr(NJ2=51, nsite=16)
+if __name__ == '__main__':
+    fire.Fire()
+
+# # depth benchmark
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 16])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 16, 8])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 32, 16, 8])
+# # run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 256, 200, 64, 32, 32, 32, 32, 32, 32, 32, 32, 16, 16, 8])
+
+# # shape benchmark
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 32, 64])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 32, 16, 32])
+# run_rtheta_mlp(J2=0.8, nsite=10, mlp_shape=[128, 64, 32, 32, 64])
+# #run_ed_msr(J2=0.8, nsite=16)
+# #scale_ed_msr(NJ2=51, nsite=16)
