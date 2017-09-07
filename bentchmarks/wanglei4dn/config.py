0dcc5c3436b3774baf8de1b89f32b2639b2fffa9
GiggleLiu <cacate0129@gmail.com>
2017-09-07 23:24:13 +0800

    ...

---
+++ bentchmarks/wanglei4dn/config.py
@@ -0,0 +1,19 @@
+from models.wanglei4 import WangLei4
+
+num_features_list = [
+        [8, 32],
+        [16, 128, 32],
+        [16, 128, 64, 32],
+        [16, 128, 64, 32, 16],
+        [32, 256, 64, 16, 8],
+        [1, 16, 256, 16],
+        [32, 64, 512, 64, 8],
+        [4, 16, 256, 16],
+        ]
+
+def modifyconfig_and_getnn(config, bentch_id):
+    nfs = num_features_list[bentch_id]
+    rbm = WangLei4(input_shape=tuple(config['hamiltonian']['size']),\
+            NF=nfs[0], K=4, num_features=nfs[1:],
+            version='conv', itype='complex128', dtype0='float64', dtype1='complex128')
+    return rbm
